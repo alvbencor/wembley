27f8bd4713fbfc4aae545c26aa97d72c9c7841b9
Alvaro Benito <44552582+alvbencor@users.noreply.github.com>
2023-07-18 13:23:03 +0200

    [ADD] Css

--- prueba-wembley/src/app/app.module.ts
+++ prueba-wembley/src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProvinciasComponent } from './provincias/provincias.component';
+import {
+  ProvinciasComponent,
+  FilterPipe,
+} from './provincias/provincias.component';
 import { ProvinciaDetalleComponent } from './provincia-detalle/provincia-detalle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProvinciasComponent,
-    ProvinciaDetalleComponent
+    ProvinciaDetalleComponent,
+    FilterPipe,
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- prueba-wembley/src/app/provincias/provincias.component.ts
+++ prueba-wembley/src/app/provincias/provincias.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter',
+})
+export class FilterPipe implements PipeTransform {
+  transform(items: any[], searchText: string): any[] {
+    if (!items || !searchText) {
+      return items;
+    }
+
+    searchText = searchText.toLowerCase();
+
+    return items.filter((item) =>
+      item.NOMBRE_PROVINCIA.toLowerCase().startsWith(searchText)
+    );
+  }
+}
 
 @Component({
   selector: 'app-provincias',
   templateUrl: './provincias.component.html',
-  styleUrls: ['./provincias.component.css']
+  styleUrls: ['./provincias.component.css'],
 })
-export class ProvinciasComponent implements OnInit{
+export class ProvinciasComponent implements OnInit {
   provincias: any[] = [];
+  filtroProvincias: string = '';
+  ordenAlfabetico: boolean = true;
+  mostrarBotonOrden: boolean = true;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.obtenerProvincias();
@@ -17,16 +39,42 @@ export class ProvinciasComponent implements OnInit{
 
   obtenerProvincias() {
     const url = 'https://www.el-tiempo.net/api/json/v2/provincias';
-    this.http.get<any>(url).subscribe(data => {
+    this.http.get<any>(url).subscribe((data) => {
       this.provincias = data.provincias;
     });
   }
 
   eliminarProvincia(provincia: any) {
-  const index = this.provincias.indexOf(provincia);
-  if (index !== -1) {
-    this.provincias.splice(index, 1);
+    const index = this.provincias.indexOf(provincia);
+    if (index !== -1) {
+      this.provincias.splice(index, 1);
+    }
   }
-}
 
+  filtrarProvincias(provincia: any): boolean {
+    return provincia.NOMBRE_PROVINCIA.toLowerCase().startsWith(
+      this.filtroProvincias.toLowerCase()
+    );
+  }
+
+  cambiarOrden() {
+    this.ordenAlfabetico = !this.ordenAlfabetico;
+    if (this.ordenAlfabetico) {
+      this.provincias.sort((a, b) =>
+        a.NOMBRE_PROVINCIA.localeCompare(b.NOMBRE_PROVINCIA)
+      );
+    } else {
+      this.provincias.sort((a, b) =>
+        b.NOMBRE_PROVINCIA.localeCompare(a.NOMBRE_PROVINCIA)
+      );
+    }
+  }
+
+  ocultarBoton() {
+    this.mostrarBotonOrden = false;
+  }
+
+  mostrarBoton() {
+    this.mostrarBotonOrden = true;
+  }
 }
